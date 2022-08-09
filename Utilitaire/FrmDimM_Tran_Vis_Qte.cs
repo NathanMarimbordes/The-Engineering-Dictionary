5dae876d49518a1e380fe46eea0eeeb66c91a116
Spherecla <jeremy.ducla@utt.fr>
2022-08-09 20:58:51 +1000

    Excel problematique
     mise en commentaire ligne qui pose pb
    dans le FrmDimM_Tran_Vis_Qte.cs

--- Utilitaire/FrmDimM_Tran_Vis_Qte.cs
+++ Utilitaire/FrmDimM_Tran_Vis_Qte.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using ExcelTDMLib;
+//using ExcelTDMLib;
 using Excel = Microsoft.Office.Interop.Excel;
 
 
