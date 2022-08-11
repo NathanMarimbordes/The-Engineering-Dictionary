f3a3c9c98c56defd9ad8e76a5968dc052560795b
Spherecla <jeremy.ducla@utt.fr>
2022-08-11 16:41:46 +1000

    ç

--- Python-Programs/Avant_test.py
+++ Python-Programs/Avant_test.py
@@ -2,20 +2,12 @@
 import matplotlib.pyplot as plt
 from HospitalApi import test
 
-d = [test.d for test in test]
-D = [test.D for test in test]
-a = [test.a for test in test]
-z = [test.z for test in test]
+class calculator:
+    def add(self, x, y):
+        return x + y
 
-
-import numpy as np 
-
-# TEST PREMIERE UTILISATION DE PYTHON DANS DU C#
-
-#On donne au début 3 variables simples.
-#Calcul rapide évidemment
-S1 =d+D
-S2 =z
-S3 =a
+    def increment(self, x):
+        x += 1;
+        return x;
 
 print('S1 = %0.6f mm\n S2 = %0.6f mm\n S3 = %0.6f mm\n'%(S1,S2,S3))
